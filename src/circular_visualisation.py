60cc2f93eb1bb71d930f9407cc2e546657cbd468
atrria <ada.lesna@gmail.com>
2021-03-23 01:05:28 +0100

    Fixed plot aspect ratio

--- src/circular_visualisation.py
+++ src/circular_visualisation.py
@@ -7,16 +7,14 @@ import numpy as np
 
 def visualise_graph_on_circle(adjacency_matrix, save_to_file = False):
     nodes_number = len(adjacency_matrix)
-
     phi = 2 * math.pi / nodes_number
-    x_start = 30
-    y_start = 30
-    graph_radius = 10
+    graph_radius = 8
+    plt.figure(figsize=(6, 6))
 
     nodes_positions = {}
 
     for node in range(nodes_number):
-        nodes_positions.update({node: (x_start + math.cos(phi * node) * graph_radius, y_start + math.sin(phi * node) * graph_radius)})
+        nodes_positions.update({node: (math.cos(phi * node) * graph_radius, math.sin(phi * node) * graph_radius)})
 
     circular_graph = netx.from_numpy_matrix(adjacency_matrix)
     netx.draw_networkx(circular_graph, nodes_positions)
@@ -28,5 +26,16 @@ def visualise_graph_on_circle(adjacency_matrix, save_to_file = False):
         plt.show()
 
 # przykladowa macierz sasiedztwa
-arr = np.matrix([[1, 1, 0, 0, 1, 0], [0, 0, 1, 0, 1, 1], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 0], [1, 1, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0]])
+arr = np.matrix([[0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+                 [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+                 [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+                 [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                 [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+                 [0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]])
 visualise_graph_on_circle(arr)
