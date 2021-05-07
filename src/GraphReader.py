18a25b3de5c6dfaf0c0d7b24ec7d6d989dc03f50
gabi15 <45560248+gabi15@users.noreply.github.com>
2021-05-07 17:20:18 +0200

    Revert "Euler cycle2"

--- src/GraphReader.py
+++ src/GraphReader.py
@@ -23,8 +23,6 @@ class GraphReader:
             return self.read_adjacency_list()
         if representation == GraphRepresentation.INCIDENCE_MATRIX:
             return self.read_incidence_matrix()
-        if representation == GraphRepresentation.GRAPHICAL_SEQUENCE:
-            return self.read_graphical_sequence()
 
     def read_from_file(self) -> np.ndarray:
         """Read a matrix from a file"""
@@ -69,41 +67,3 @@ class GraphReader:
                                               GraphRepresentation.ADJACENCY_MATRIX)
         return matrix
 
-    def is_graphical_sequence(self, graphical_sequence):
-        n = len(graphical_sequence)
-        graph_seq = sorted(graphical_sequence, reverse=True)
-
-        if sum(i % 2 for i in graph_seq) % 2 or graph_seq[n - 1] < 0 or graph_seq[0] >= n:
-            return False
-
-        flag = 0
-        for i in range(0, n):
-            if graph_seq[i] == 0:
-                flag += 1
-        if flag == n:
-            return True
-
-        for i in range(1, graph_seq[0] + 1):
-            if graph_seq[i] > 0:
-                graph_seq[i] -= 1
-            else:
-                return False
-        graph_seq[0] = 0
-
-        return self.is_graphical_sequence(graph_seq)
-
-    def read_graphical_sequence(self):
-        graphical_sequence = []
-        try:
-            with open(self.filename) as f:
-                line = f.readline()
-                elements = [int(item.strip()) for item in line.split(" ") if line.strip()]
-                graphical_sequence.append(elements)
-        except Exception as e:
-            raise IncorrectInputException("Incorrect input - an error occurred while reading the file:\n" + str(e))
-        if len(graphical_sequence) == 0:
-            raise IncorrectInputException("Incorrect input - graphical sequence is empty")
-        elif not self.is_graphical_sequence(graphical_sequence[0]):
-            raise IncorrectInputException("Incorrect input - sequence is not graphical")
-        else:
-            return GraphConverter.convert_graph(GraphConverter.convert_graph(graphical_sequence[0], GraphRepresentation.GRAPHICAL_SEQUENCE, GraphRepresentation.ADJACENCY_LIST), GraphRepresentation.ADJACENCY_LIST, GraphRepresentation.ADJACENCY_MATRIX)
