a6fabd01a463563ae231f9c32082149909b5da45
Aleksandra Kuś <aleksandra0kus@gmail.com>
2021-05-09 22:29:35 +0200

    small naming changes

--- src/MinimumSpanningTree.py
+++ src/MinimumSpanningTree.py
@@ -4,29 +4,25 @@ from GraphRepresentation import GraphRepresentation
 
 def minimum_spanning_tree(matrix: np.ndarray):
     """Find a minimum spanning tree of a given tree using Prims algorithm and return its adjacency matrix"""
-    original = matrix.astype('float64')
-    original[matrix == 0] = np.nan
-    n = np.size(original, axis=1)
+    copy = matrix.astype('float64')
+    copy[matrix == 0] = np.nan
+    n = np.size(copy, axis=1)
     T = []
-    W = []
-    mat = np.zeros((n, n), dtype=int)
+    mat = np.zeros(matrix.shape, dtype=int)
 
     T.append(0)
-    original[:, 0] = np.nan
-
-    for i in range(1, n):
-        W.append(i)
+    copy[:, 0] = np.nan
+    W = [i for i in range(1, n)]
 
     while W:
-        mask = np.zeros(original.shape, dtype=np.bool)
+        mask = np.zeros(matrix.shape, dtype=np.bool)
         mask[T] = True
-        min_weight = np.nanargmin(original[mask])
+        min_weight = np.nanargmin(copy[mask])
         i = min_weight // n
         j = min_weight % n
         T.append(j)
         W.remove(j)
-        original[:, j] = np.nan
+        copy[:, j] = np.nan
         mat[i][j] = matrix[i][j]
 
     return mat, GraphRepresentation.ADJACENCY_MATRIX
-
