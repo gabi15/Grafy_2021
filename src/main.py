1b5b3b7af948ffeb1a6415e05ce5b8442ac1bd4a
atrria <ada.lesna@gmail.com>
2021-05-09 22:11:34 +0200

    Removed not needed functionalities

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 import sys
 import argparse
+from copy import deepcopy
 
 from Graph import Graph
-from RandomGraphGenerator import *
 from RandomWeightedGraphGenerator import *
 from MinimumSpanningTree import *
 
 
 def save_graph() -> None:
     representation = input("Select an output representation:\n"
-                               "1 - Adjacency matrix\n"
-                               "2 - Adjacency list\n"
-                               "3 - Incidence matrix\n"
-                               )
+                           "2 - Adjacency list\n"
+                           "3 - Incidence matrix\n"
+                           )
     if representation in ["1", "2", "3"]:
         filename = input("Insert an output filename:\n")
         perform_save(int(representation), filename)
@@ -50,57 +49,19 @@ def draw_graph() -> None:
 
 def generate_graph() -> None:
     print("Select number of vertices and probability for the G(n,p) weighted graph:")
-    n = input("Enter the number of graph vertices:\n")
-    p = input("Enter the probability in range [0,1]:\n")
+    n = int(input("Enter the number of graph vertices:\n"))
+    p = float(input("Enter the probability in range [0,1]:\n"))
     try:
-        graph.set_graph(random_weighted_graph_probability(int(n), float(p)))
+        graph.set_graph(random_weighted_graph_probability(n, p))
+        while max(graph.find_components()) > 1:
+            graph.set_graph(random_weighted_graph_probability(n, p))
     except Exception as e:
         print("Error: " + str(e) + "\nPlease try again\n")
         generate_graph()
 
 
-def read_graph() -> None:
-    representation = input("Enter a representation of the input:\n"
-                           "1 - Adjacency matrix\n"
-                           "2 - Adjacency list\n"
-                           "3 - Incidence matrix\n"
-                           "Press any other key to return to the main menu\n"
-                           )
-    if representation in ["1", "2", "3"]:
-        filename = input("Enter the name of the input file (stored in folder data):\n")
-        if not perform_read(int(representation), filename):
-            read_graph()
-    else:
-        main()
-
-
-def perform_read(representation: int, filename: str) -> bool:
-    try:
-        graph.read_data(GraphRepresentation(representation), filename)
-        return True
-    except Exception as e:
-        print("Error: " + str(e) + "\nPlease try again\n")
-        return False
-
-
 parser = argparse.ArgumentParser(description='Command line interface for graph app.')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--read',
-                   action='store',
-                   nargs=2,
-                   metavar=('<filename>', '<representation>'),
-                   help='Read input from a file with given name and representation.'
-                        'File has to be stored in folder data. Available input representations:\n'
-                        '1 - adjacency matrix,\n '
-                        '2 - adjacency list, \n'
-                        '3 - incidence matrix')
-
-group.add_argument('--generate-nl',
-                   nargs=2,
-                   metavar=('<nvertices>', '<nedges>'),
-                   type=int,
-                   action='store',
-                   help='Generate an nl graph.')
 group.add_argument('--generate-np',
                    nargs=2,
                    metavar=('<nvertices>', '<probability>'),
@@ -130,41 +91,19 @@ parser.add_argument('--save',
 
 def run_cmd_app(args):
     print(args)
-    if args.read is not None:
-        if args.read[1] in ['1', '2', '3']:
-            representation = int(args.read[1])
-            filename = args.read[0]
-            if not perform_read(representation, filename):
-                sys.exit(1)
-    elif args.generate_nl is not None:
-        try:
-            n = int(args.generate_nl[0])
-            l = int(args.generate_nl[1])
-            graph.set_graph(random_graph_edges(n, l))
-        except Exception as e:
-            print("Error: " + str(e) + "\nPlease try again\n")
-            sys.exit(1)
-    elif args.generate_np is not None:
+    if args.generate_np is not None:
         try:
             n = int(args.generate_np[0])
             p = float(args.generate_np[1])
-            graph.set_graph(random_graph_probability(n, p))
+            graph.set_graph(random_weighted_graph_probability(n, p))
+            while max(graph.find_components()) > 1:
+                graph.set_graph(random_weighted_graph_probability(n, p))
         except Exception as e:
             print("Error: " + str(e) + "\nPlease try again\n")
             sys.exit(1)
     else:
         print("Data input option not specified. Please specify --read or --generate option\n")
         sys.exit(1)
-    if args.draw is not None:
-        if args.draw is not True:
-            file_name = args.draw
-            try:
-                graph.visualise_graph_on_circle(True, file_name)
-            except Exception as e:
-                print("Error: " + str(e) + "\nPlease try again\n")
-                sys.exit(1)
-        else:
-            graph.visualise_graph_on_circle(False, None)
     if args.save is not None:
         if args.save[1] in ["1", "2", "3"]:
             representation = int(args.save[1])
@@ -202,8 +141,10 @@ def find_minimax_center():
 
 
 def find_minimal_spanning_tree():
-    graph.set_graph(minimum_spanning_tree(graph.adjacency_matrix))
-    graph.visualize_graph_with_weights(False, "")
+    graph_copy = deepcopy(graph)
+    graph_copy.set_graph(minimum_spanning_tree(graph_copy.adjacency_matrix))
+    graph_copy.visualize_graph_with_weights(False, "")
+    del graph_copy
 
 
 def main() -> None:
@@ -211,49 +152,37 @@ def main() -> None:
         args = parser.parse_args()
         run_cmd_app(args)
     else:
-        job = input("Select an option:\n"
-                    "1 - Read the graph from file \n"
-                    "2 - Generate a random graph \n"
-                    )
-
-        if job in ["1", "2"]:
-            if job == "1":
-                read_graph()
-            if job == "2":
-                generate_graph()
-            while True:
-                job = input("Choose what you want to do with the graph:\n"
-                            "1 - Save the graph to the file\n"
-                            "2 - Draw the graph\n"
-                            "3 - Find shortest paths\n"
-                            "4 - Find distances matrix\n"
-                            "5 - Find center\n"
-                            "6 - Find minimax center\n"
-                            "7 - Find minimal spanning tree\n"
-                            "8 - Exit the program\n"
-                            "Press any other key to return to the main menu\n")
-                if job in ["1", "2", "3", "4", "5", "6", "7", "8"]:
-                    if job == "1":
-                        save_graph()
-                    elif job == "2":
-                        draw_graph()
-                    elif job == "3":
-                        find_shortest_paths()
-                    elif job == "4":
-                        find_distances_matrix()
-                    elif job == "5":
-                        find_center()
-                    elif job == "6":
-                        find_minimax_center()
-                    elif job == "7":
-                        find_minimal_spanning_tree()
-                    elif job == "8":
-                        sys.exit(1)
-                else:
-                    main()
-        else:
-            print("Wrong option selected, try again")
-            main()
+        generate_graph()
+        while True:
+            job = input("Choose what you want to do with the graph:\n"
+                        "1 - Save the graph to the file\n"
+                        "2 - Draw the graph\n"
+                        "3 - Find shortest paths\n"
+                        "4 - Find distances matrix\n"
+                        "5 - Find center\n"
+                        "6 - Find minimax center\n"
+                        "7 - Find minimal spanning tree\n"
+                        "8 - Exit the program\n"
+                        "Press any other key to return to the main menu\n")
+            if job in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+                if job == "1":
+                    save_graph()
+                elif job == "2":
+                    draw_graph()
+                elif job == "3":
+                    find_shortest_paths()
+                elif job == "4":
+                    find_distances_matrix()
+                elif job == "5":
+                    find_center()
+                elif job == "6":
+                    find_minimax_center()
+                elif job == "7":
+                    find_minimal_spanning_tree()
+                elif job == "8":
+                    sys.exit(1)
+            else:
+                main()
 
 
 if __name__ == "__main__":
