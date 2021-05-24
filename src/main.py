48e57b9f2e03f167282b9e68cbf9c977f258b2df
Gabriela Leśniak <gabcia100@gmail.com>
2021-05-24 15:22:06 +0200

    taking layers as input

--- src/main.py
+++ src/main.py
@@ -23,7 +23,17 @@ def draw_graph_with_flow() -> None:
         draw_graph_with_flow()
 
 
+def get_layers():
+    layers = input("How many layers between source and sink do uou want? Number must be between 2 and 4\n")
+    if layers in ["2","3","4"]:
+        return int(layers)
+    else:
+        print("Wrong input, try again")
+        return get_layers()
+
+
 def draw_graph() -> None:
+    layers = get_layers()
     save_to_file = input("Do you want to save image to a file? Select an option:\n"
                              "0 - No\n"
                              "1 - Yes\n"
@@ -35,7 +45,7 @@ def draw_graph() -> None:
                 file_name = input("Specify file name for the graph:\n")
             except Exception as e:
                 print("Error: " + str(e) + "\nPlease try again\n")
-        multi_dict = generate_random_flow_graph(2)
+        multi_dict = generate_random_flow_graph(layers)
         graph = prepare_multilayer_graph(multi_dict[0], multi_dict[1])
         flow_graph.set_flow_dict(multi_dict[0])
         flow_graph.set_layers(multi_dict[1])
@@ -55,7 +65,7 @@ def main() -> None:
             draw_graph()
         while True:
             job = input("Choose what you want to do with the graph:\n"
-                            "1 - find max flow\n"
+                            "1 - Find max flow\n"
                             "2 - Exit the program\n"
                             "Press any other key to return to the main menu\n")
             if job in ["1", "2"]:
