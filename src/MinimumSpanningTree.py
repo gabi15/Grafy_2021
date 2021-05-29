6cbb6e6c5b78a1d06b62d6aa87f4e3f3c94e6f9d
Aleksandra Kuś <aleksandra0kus@gmail.com>
2021-05-29 14:48:04 +0200

    fixed mst

--- src/MinimumSpanningTree.py
+++ src/MinimumSpanningTree.py
@@ -2,27 +2,26 @@ import numpy as np
 from GraphRepresentation import GraphRepresentation
 
 
-def minimum_spanning_tree(matrix: np.ndarray):
+def minimum_spanning_tree(adjacency_matrix: np.ndarray):
     """Find a minimum spanning tree of a given tree using Prims algorithm and return its adjacency matrix"""
-    copy = matrix.astype('float64')
-    copy[matrix == 0] = np.nan
-    n = np.size(copy, axis=1)
-    T = []
-    mat = np.zeros(matrix.shape, dtype=int)
-
-    T.append(0)
-    copy[:, 0] = np.nan
+    n = np.size(adjacency_matrix, axis=1)
+    T = [0]
     W = [i for i in range(1, n)]
 
-    while W:
-        mask = np.zeros(matrix.shape, dtype=np.bool)
-        mask[T] = True
-        min_weight = np.nanargmin(copy[mask])
-        i = min_weight // n
-        j = min_weight % n
-        T.append(j)
-        W.remove(j)
-        copy[:, j] = np.nan
-        mat[i][j] = matrix[i][j]
+    matrix = np.zeros((n, n), dtype=int)
+    v_x = 0
+    v_y = 0
 
-    return mat, GraphRepresentation.ADJACENCY_MATRIX
+    while W:
+        min_v = np.inf
+        for v in T:
+            for u in W:
+                if adjacency_matrix[v][u] != 0 and adjacency_matrix[v][u] < min_v:
+                    min_v = adjacency_matrix[v][u]
+                    v_x = v
+                    v_y = u
+        T.append(v_y)
+        W.remove(v_y)
+        matrix[v_x][v_y] = matrix[v_y][v_x] = adjacency_matrix[v_x][v_y]
+
+    return matrix, GraphRepresentation.ADJACENCY_MATRIX
